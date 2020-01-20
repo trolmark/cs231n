5f990b20b86b6ae8218c70e0dc1904d5c5f52458
Andrew Denisov <trolmark@gmail.com>
2020-01-20 07:21:20 +0200

    Assignment_1 : SVM, gradient descent

--- Assignment_1/cs231n/classifiers/linear_svm.py
+++ Assignment_1/cs231n/classifiers/linear_svm.py
@@ -22,7 +22,7 @@ def svm_loss_naive(W, X, y, reg):
     - gradient with respect to weights W; an array of same shape as W
     """
     dW = np.zeros(W.shape) # initialize the gradient as zero
-
+    
     # compute the loss and the gradient
     num_classes = W.shape[1]
     num_train = X.shape[0]
@@ -30,12 +30,16 @@ def svm_loss_naive(W, X, y, reg):
     for i in range(num_train):
         scores = X[i].dot(W)
         correct_class_score = scores[y[i]]
+        scale_w = 0
         for j in range(num_classes):
             if j == y[i]:
                 continue
             margin = scores[j] - correct_class_score + 1 # note delta = 1
+            dW[:,j] += X[i] if margin > 0 else 0
             if margin > 0:
+                scale_w += 1
                 loss += margin
+        dW[:,y[i]] += -scale_w*X[i]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -54,8 +58,8 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
-
+    dW /= num_train
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     
     return loss, dW
