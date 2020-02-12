A_1 : fix backprop gradients for layers

Assignment_1/cs231n/classifiers/neural_net.py
@@ -80,7 +80,7 @@ class TwoLayerNet(object):
         #############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         
-        first_layer = X.dot(W1) + b1
+        first_layer = np.dot(X, W1) + b1
         re_lu = lambda x: np.maximum(0,x)
         hidden_layer = re_lu(first_layer)
         scores = hidden_layer.dot(W2) + b2
@@ -107,7 +107,7 @@ class TwoLayerNet(object):
         
         loss = scores_y + np.log(scores_j)
         loss = np.mean(loss)
-        loss += reg * np.sum(W1 * W1) + reg * np.sum(W2 * W2)
+        loss += reg * (np.sum(W1 * W1) + np.sum(W2 * W2) + np.sum(b1*b1) + np.sum(b2*b2))
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -122,22 +122,23 @@ class TwoLayerNet(object):
 
         derivative = np.exp(scores) / np.matrix(scores_j).T
         derivative[np.arange(N), y] -= 1
+        derivative /= N 
         
         dW2 = hidden_layer.T.dot(derivative)
-        dW2 /= N
-        dW2 += reg * W2
+        dW2 += 2*reg * W2
         
         # backprop into hidden layer
-        dhidden = np.dot(derivative, dW2.T)       
+        dhidden = np.dot(derivative, W2.T)       
         # backprop the ReLU non-linearity
         dhidden[hidden_layer <= 0] = 0
         
         dW1 = X.T.dot(dhidden)
-        dW1 /= N
-        dW1 += reg * W1
+        dW1 += 2*reg * W1
                
         grads['W2'] = dW2
+        grads['b2'] = np.sum(derivative, axis=0) # TODO : array of array
         grads['W1'] = dW1  
+        grads['b1'] = np.sum(dhidden, axis=0)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -182,7 +183,9 @@ class TwoLayerNet(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            batch_indices = np.random.choice(np.arange(num_train),size = batch_size, replace = True)
+            X_batch = X[batch_indices]
+            y_batch = y[batch_indices]
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -198,7 +201,10 @@ class TwoLayerNet(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            self.params['W1'] += -learning_rate * grads['W1']
+            self.params['b1'] += -learning_rate * grads['b1']
+            self.params['W2'] += -learning_rate * grads['W2']
+            self.params['b2'] += -learning_rate * grads['b2']
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -244,7 +250,7 @@ class TwoLayerNet(object):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        y_pred = np.argmax(X.dot(self.params['W1']), axis = 1)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 